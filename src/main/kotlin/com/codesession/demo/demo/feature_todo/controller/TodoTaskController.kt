981278fa0d74437208880e259bc923b3d408fe7f
Mansoor Nisar <126686934+mansoornisar10p@users.noreply.github.com>
2024-03-15 15:54:32 +0500

    Added Autowired annotation

--- src/main/kotlin/com/codesession/demo/demo/feature_todo/controller/TodoTaskController.kt
+++ src/main/kotlin/com/codesession/demo/demo/feature_todo/controller/TodoTaskController.kt
@@ -2,11 +2,12 @@ package com.codesession.demo.demo.feature_todo.controller
 
 import com.codesession.demo.demo.feature_todo.service.TodoTaskServiceImpl
 import com.codesession.demo.demo.feature_todo.model.TodoTask
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/tasks")
-class TodoTaskController(private val service: TodoTaskServiceImpl) {
+class TodoTaskController @Autowired constructor(private val service: TodoTaskServiceImpl) {
 
     @GetMapping
     fun getAllTasks(
