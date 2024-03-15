963d8c94358e3c428904b704070444346e9fc41c
Mansoor Nisar <126686934+mansoornisar10p@users.noreply.github.com>
2024-03-15 16:14:08 +0500

    moved Main application to -> main package

--- src/main/kotlin/com/codesession/demo/demo/main/DemoApplication.java
+++ src/main/kotlin/com/codesession/demo/demo/main/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.codesession.demo.demo;
+package com.codesession.demo.demo.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
